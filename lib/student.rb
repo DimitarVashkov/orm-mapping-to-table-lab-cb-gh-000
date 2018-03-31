048fef7128ccca71386ea5761dcf08f6c0f89102
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-31 19:52:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,10 +28,17 @@ class Student
 
   def save
     sql =  <<-SQL
-    INSERT INTO students (name, grade) 
+    INSERT INTO students (name, grade)
     VALUES (?, ?)
         SQL
     DB[:conn].execute(sql,self.name,self.grade)
   end
+
+  def self.create(name:, grade:)
+    student = Student.new(name,grade)
+    student.save
+    student
+  end
+  
 
 end
